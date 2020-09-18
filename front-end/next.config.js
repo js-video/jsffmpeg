48de819388c79f8eb183ed75c5d56993afd81e0e
fay <869770665@qq.com>
2020-09-18 12:21:13 +0800

    upd

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -2,7 +2,7 @@
  * Create by fay on 4/27/20 11:59 下午
  */
 const withNextConfig = require('@fay-next/config/next/config');
-const assetPrefix = process.env.NODE_ENV === 'production' ? '' : '';
+const assetPrefix = '';
 
 module.exports = withNextConfig({
   assetPrefix,
